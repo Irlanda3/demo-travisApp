9fb50ec673120e771ecefddeecbe1175bbbe7f60
anayeli ochoa <ochoa@example.com>
2020-01-31 13:07:10 -0700

    .py

---
+++ installingGitHub.py
@@ -0,0 +1,18 @@
+'''https://github.com/Irlanda3/demo-travisApp.git
+
+1 baja git
+2 https://git-scm.com/downloads
+3  intalalo y abre otra vez visual studio code
+4 fui a mi github e hice un repositorio nuevo luego copie el link y lo puse
+en el control shift p  pero le puse git clone y dentro de ese repositorio abri installingGitHub.py
+
+5 luego me pide escoger el repository location
+6 agrega new file que se llame .travis.yml
+7  https://blog.travis-ci.com/2019-08-07-extensive-python-testing-on-travis-ci
+8 https://www.youtube.com/watch?v=BFpSD2eoXUk
+    
+    git add .
+    git commit -m "updated"
+    git push origin master
+'''
+print("Hello enero")
